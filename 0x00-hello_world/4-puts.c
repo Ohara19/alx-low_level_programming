6d98557c51fd51ce0c472a44020d89c752012a0b
ohara19 <oharatottenham@yahoo.com>
2022-10-13 19:01:53 -0700

    t4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,21 +1,12 @@
 #include <stdio.h>
-
 /**
+ * main - funtion puts
  *
- *  *  main -Entry point
- *
- *   *
- *
- *    *  Return: Always 0 (Success)
- *
- *     */
+ * return: 0
+ */
 
 int main(void)
-
 {
-
-		puts("\"Programming is like building a multilingual puzzle");
-
-			return (0);
-
+	puts("\"programming is like building a multilingual puzzle");
+	return (0);
 }
